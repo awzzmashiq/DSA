4290a9f26ad900be6d87e4c8c15e0d1ea080518d
Mohamed Asik Sapteen <36636681+awzzmashiq@users.noreply.github.com>
2025-10-30 00:31:22 -0400

    Time: 13 ms (87.70%) | Memory: 58.4 MB (46.51%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+
+        HashSet<Integer> set= new HashSet<Integer>();
+
+        for(int num : nums){
+
+            if(!set.contains(num))
+            {
+                set.add(num);
+            }
+            else{
+                return true;
+            }
+
+        }
+        return false;
+        
+    }
+}
